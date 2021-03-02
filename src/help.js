b73c245236dfc0b25e29c5fdb1365f8024b75bd7
* <9282892>
2021-03-02 03:45:36 -0300

    First commit

---
+++ src/help.js
@@ -0,0 +1,138 @@
+const help = (prefix) => {
+	return `
+「 *₳ⱠɄ₵₳ⱤĐ* 」
+
+  ❍ *Informações*
+  ❍ Prefix: 「  ${prefix}  」
+  ❍ Dono : ₳ⱠɄ₵₳ⱤĐ
+  ❍ Canal do dono: https://youtube.com/channel/UCx9xDaEvh3G6N4I4-VhYqVg
+
+❍ ꜱᴏʙʀᴇ
+  
+  ║➩ ❍ ${prefix}info
+  ║➩ ❍ ${prefix}blocklist
+  ║➩ ❍ ${prefix}chatlist
+  ║➩ ❍ ${prefix}ping
+  ║➩ ❍ ${prefix}bugreport
+❍ ꜰᴀᴢᴇʀ
+  
+  ║➩ ❍ ${prefix}figu
+  ║➩ ❍ ${prefix}toimg
+  ║➩ ❍ ${prefix}virarmp3
+  ║➩ ❍ ${prefix}bpink
+  ║➩ ❍ ${prefix}marvellogo
+  ║➩ ❍ ${prefix}snowwrite
+  ║➩ ❍ ${prefix}3dtext
+  ║➩ ❍ ${prefix}ninjalogo
+  ║➩ ❍ ${prefix}water
+  ║➩ ❍ ${prefix}firetext
+  ║➩ ❍ ${prefix}logowolf
+  ║➩ ❍ ${prefix}logowolf2
+  ║➩ ❍ ${prefix}phlogo
+  ║➩ ❍ ${prefix}glitch
+  ║➩ ❍ ${prefix}neonlogo
+  ║➩ ❍ ${prefix}neonlogo2
+  ║➩ ❍ ${prefix}lionlogo
+  ║➩ ❍ ${prefix}jokerlogo
+  ║➩ ❍ ${prefix}shadow
+  ║➩ ❍ ${prefix}burnpaper
+  ║➩ ❍ ${prefix}coffee
+  ║➩ ❍ ${prefix}lovepaper
+  ║➩ ❍ ${prefix}woodblock
+  ║➩ ❍ ${prefix}qowheart
+  ║➩ ❍ ${prefix}mutgrass
+  ║➩ ❍ ${prefix}undergocean
+  ║➩ ❍ ${prefix}woodenboards
+  ║➩ ❍ ${prefix}wolfmetal
+  ║➩ ❍ ${prefix}metalictglow
+  ║➩ ❍ ${prefix}8bit
+  ║➩ ❍ ${prefix}ttp
+  ║➩ ❍ ${prefix}herrypotter
+  ║➩ ❍ ${prefix}pubglogo
+  ║➩ ❍ ${prefix}quotemaker
+  ║➩ ❍ ${prefix}textoft
+  ║➩ ❍ ${prefix}calunia
+  ║➩ ❍ ${prefix}shota
+  ║➩ ❍ ${prefix}dere
+❍ ᴍᴇᴅɪᴀ
+  
+  ║➩ ❍ ${prefix}trendtwit
+  ║➩ ❍ ${prefix}randomkpop
+  ║➩ ❍ ${prefix}ytsearch
+❍ ꜰᴀʟᴀʀ ᴄᴏᴍ ᴏ ʙᴏᴛ
+  
+  ║➩ ❍ ${prefix}chatAlucard
+  ║➩ ❍ ${prefix}whatday
+  ║➩ ❍ ${prefix}avalie
+  ║➩ ❍ ${prefix}chatpode
+❍ ᴅᴏᴡɴʟᴏᴀᴅ
+  
+  ║➩ ❍ ${prefix}images
+  ║➩ ❍ ${prefix}ytmp3
+  ║➩ ❍ ${prefix}ytmp4
+  ║➩ ❍ ${prefix}tiktok
+  ║➩ ❍ ${prefix}joox
+❍ ᴍᴇᴍᴇ
+  
+  ║➩ ❍ ${prefix}meme
+  ║➩ ❍ ${prefix}memeindo
+❍ ꜱᴏᴍ
+  
+  ║➩ ❍ ${prefix}musica
+  ║➩ ❍ ${prefix}audio
+❍ ᴍᴜꜱɪᴄᴀ
+  
+  ║➩ ❍ ${prefix}lirik
+  ║➩ ❍ ${prefix}chord
+❍ ꜱᴛᴀʟᴋᴇᴀʀ
+  
+  ║➩ ❍ ${prefix}tiktokstalk
+  ║➩ ❍ ${prefix}igstalk
+❍ ᴀɴɪᴍᴇꜱ
+  
+  ║➩ ❍ ${prefix}neonime
+  ║➩ ❍ ${prefix}pokemon
+  ║➩ ❍ ${prefix}loli
+  ║➩ ❍ ${prefix}waifu
+  ║➩ ❍ ${prefix}randomanime
+  ║➩ ❍ ${prefix}husbu
+  ║➩ ❍ ${prefix}husbu2
+  ║➩ ❍ ${prefix}buscanime
+  ║➩ ❍ ${prefix}nekonime
+❍ ᴇɴᴛʀᴇᴛᴇɴɪᴍᴇɴᴛᴏꜱ
+  
+  ║➩ ❍ ${prefix}alay
+  ║➩ ❍ ${prefix}gantengcek
+  ║➩ ❍ ${prefix}watak
+  ║➩ ❍ ${prefix}hobby
+  ║➩ ❍ ${prefix}game
+  ║➩ ❍ ${prefix}bucin
+  ║➩ ❍ ${prefix}trust
+  ║➩ ❍ ${prefix}dare
+  ║➩ ❍ ${prefix}simi
+❍ ᴄᴏᴍᴀɴᴅᴏꜱ ᴅᴇ ᴀᴜᴅɪᴏ
+  
+  ║➩ ❍ ${prefix}bahasa
+❍ ᴅᴏɴᴏ
+  
+  ║➩ ❍ ${prefix}setprefix
+  ║➩ ❍ ${prefix}block
+  ║➩ ❍ ${prefix}bc
+  ║➩ ❍ ${prefix}bcgc
+  ║➩ ❍ ${prefix}clone
+  ║➩ ❍ ${prefix}clearall
+❍ ᴏᴜᴛʀᴏꜱ
+ 
+  ║➩ ❍ ${prefix}send
+  ║➩ ❍ ${prefix}wame
+  ║➩ ❍ ${prefix}virtex
+  ║➩ ❍ ${prefix}exe
+  ║➩ ❍ ${prefix}qrcode
+  ║➩ ❍ ${prefix}afk
+  ║➩ ❍ ${prefix}timer
+  ║➩ ❍ ${prefix}fml
+  ║➩ ❍ ${prefix}fml2
+`
+}
+
+exports.help = help
---
+++ src/zeusmenu.js
@@ -0,0 +1,12 @@
+const alucardmenu = (prefix, pushname) => {
+    return `❍ *Comandos do ₳ⱠɄ₵₳ⱤĐ *
+    
+    ║➩ ❍ ${prefix}setprefix
+    ║➩ ❍ ${prefix}block
+    ║➩ ❍ ${prefix}tm
+    ║➩ ❍ ${prefix}tmctt
+    ║➩ ❍ ${prefix}clearall`
+
+}
+
+exports.alucardmenu = alucardmenu
